bd7b1ccbebcde4614b9ec6b84e8122890b22daf9
cutetigress <74666402+cutetigress@users.noreply.github.com>
2023-09-04 16:06:48 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+data = pd.read_excel('C:/Users/Administrator/Desktop/RightelInternet.xlsx')
+question = data["assistant_question"]
+yes_answer = data["user_yes_answer"]
+no_answer = data["user_no_answer"]
+step_yes = data["step_if_answer_is_yes"]
+step_no = data["step_if_answer_is_no"]
+f = data["is_answer"]
+
+h = question.count()
+print(question[0])
+
+
+for i in range(0, h):
+
+    if f[i] == 0:
+
+        answer = input(" لطفا جواب بله یا خیر را وارد نمایید   ")
+        if answer == "بله":
+            count1 = step_yes[i]
+            print("**********************")
+            print(step_yes[i])
+            print("**********************")
+            print(question[count1 - 1])
+            if f[count1 - 1] == 0:
+                continue
+            else:
+                print(question[i])
+                key1 = False
+                break
+
+        elif answer == "خیر":
+                count2 = step_no[i]
+                print("**********************")
+                print(step_no[i])
+                print("**********************")
+                print(question[count2 - 1])
+                if f[count2 - 1] == 0:
+                    continue
+                else:
+                    print(question[i])
+                    key1 = False
+                    break
+
+        else:
+            print("enter correct answer")
+            break
+print("موفق باشید")
+
